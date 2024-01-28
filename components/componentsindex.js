b3a5def04a39b899932d180f82499c9abe1a251a
Luke Song <hyeonminsong7@gmail.com>
2024-01-27 22:59:46 -0800

    :sparkles: Added NFT Slider Component, :iphone: Added Responsive design Adjustments

--- components/componentsindex.js
+++ components/componentsindex.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer";
 import NavBar from "./NavBar/NavBar";
 import HeroSection from "./HeroSection/HeroSection";
 import Service from "./Service/Service";
+import BigNFTSilder from "./BigNFTSilder/BigNFTSilder";
 
-export { Button, Footer, NavBar, HeroSection, Service };
+export { Button, Footer, NavBar, HeroSection, Service, BigNFTSilder };
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
-import { HeroSection, Service } from "../components/componentsindex";
+import {
+  HeroSection,
+  Service,
+  BigNFTSilder,
+} from "../components/componentsindex";
+
+import Style from "../styles/index.module.css";
 
 const Home = () => {
   return (
-    <div>
+    <div className={Style.homePage}>
       <HeroSection />
       <Service />
+      <BigNFTSilder />
     </div>
   );
 };
