1a6faa8b5058453bf179c13137ffa93f9c82d1fa
Luke Song <hyeonminsong7@gmail.com>
2024-01-29 21:02:23 -0800

    :lipstick: Added Search Page, :sparkles: Added SearchBar Component

---
+++ pages/searchPage.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+//INTRNAL IMPORT
+import Style from "../styles/searchPage.module.css";
+import { Slider, Brand } from "../components/componentsindex";
+import { SearchBar } from "../SearchPage/searchBarIndex";
+import { Filter } from "../components/componentsindex";
+
+import { NFTCardTwo } from "../collectionPage/collectionIndex";
+import images from "../img";
+
+const searchPage = () => {
+  const collectionArray = [
+    images.nft_image_1,
+    images.nft_image_2,
+    images.nft_image_3,
+    images.nft_image_1,
+    images.nft_image_2,
+    images.nft_image_3,
+    images.nft_image_1,
+    images.nft_image_2,
+  ];
+  return (
+    <div className={Style.searchPage}>
+      <SearchBar />
+      <Filter />
+      <NFTCardTwo NFTData={collectionArray} />
+      <Slider />
+      <Brand />
+    </div>
+  );
+};
+
+export default searchPage;
